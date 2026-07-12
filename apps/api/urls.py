5b952f20f8761ca6e471df68e910a312bde718d5
joqd <gorgdis@gmail.com>
2026-07-12 15:00:01 +0330

    feat: replace SpectacularAPIView with CustomSpectacularAPIView to exclude schema from documentation

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path, include
 
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from drf_spectacular.utils import extend_schema
+
+
+@extend_schema(exclude=True)
+class CustomSpectacularAPIView(SpectacularAPIView):
+    pass
+
 
 urlpatterns = [
-	path('api/zschema/', SpectacularAPIView.as_view(), name='schema'),
+	path('api/schema/', CustomSpectacularAPIView.as_view(), name='schema'),
     path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
 	path('api/auth/', include('apps.accounts.urls')),
 	path('api/', include('apps.catalog.urls')),
