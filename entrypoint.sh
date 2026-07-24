ac815e34952db1dd12259b863eb7bc16aeff42f7
joqd <gorgdis@gmail.com>
2026-07-24 15:31:49 +0330

    feat: add static files collection to entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 echo "Running migrations..."
 python manage.py migrate --noinput
 
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
 echo "Compiling messages..."
 python manage.py compilemessages
 
