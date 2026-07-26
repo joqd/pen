d8dbcd57c4ea9205c66a569d6da04dfe8c0b8774
joqd <gorgdis@gmail.com>
2026-07-26 14:27:13 +0330

    feat: add imports for cart and wishlist serializers and services

---
+++ apps/orders/serializers/__init__.py
@@ -0,0 +1,2 @@
+from .cart_serializer import * # noqa: F403
+from .wishlist_serializer import * # noqa: F403
---
+++ apps/orders/services/__init__.py
@@ -0,0 +1,2 @@
+from .cart_service import * # noqa: F403
+from .wishlist_service import * # noqa: F403
