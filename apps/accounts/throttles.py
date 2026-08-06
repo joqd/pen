c8b892297b975dd6acd139c89d4d3a57a516a1fd
joqd <gorgdis@gmail.com>
2026-08-06 17:24:27 +0330

    feat(throttles): implement custom throttling exception and update related messages

--- apps/accounts/throttles.py
+++ apps/accounts/throttles.py
@@ -3,6 +3,8 @@ import math
 from django.utils.translation import gettext_lazy as _
 from rest_framework.exceptions import Throttled
 from rest_framework.throttling import AnonRateThrottle, SimpleRateThrottle
+from rest_framework.exceptions import APIException
+from rest_framework import status
 
 
 class OTPPhoneRateThrottle(SimpleRateThrottle):
@@ -26,14 +28,18 @@ class OTPRequestAnonRateThrottle(AnonRateThrottle):
     scope = 'otp_request_ip'
 
 
+class CustomThrottled(APIException):
+    status_code = status.HTTP_429_TOO_MANY_REQUESTS
+    default_code = "throttled"
+
 class OTPThrottledMixin:
     def throttled(self, request, wait):
         if wait is not None:
-            wait_seconds = math.ceil(wait)
-            detail = _('Too many requests. Please try again in %(wait)d seconds.') % {
-                'wait': wait_seconds,
-            }
+            wait = math.ceil(wait)
+            detail = _(
+                "Too many requests. Please try again in %(wait)d seconds."
+            ) % {"wait": wait}
         else:
-            detail = _('Too many requests. Please try again later.')
+            detail = _("Too many requests. Please try again later.")
 
-        raise Throttled(wait=wait, detail=detail, code='throttled')
+        raise CustomThrottled(detail=detail)
